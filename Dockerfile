58ce7b997086b7aa89cf5c361ecaed1c913fa4d6
Martin Rauscher <hades32@gmail.com>
2018-11-04 11:28:45 +0100

    get parameters from env

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM node
 WORKDIR /app
 CMD node index.js
+ENV GW_IDENTITY = "TODO"
+ENV GW_PSK = "TODO"
+ENV GW_ADDRESS = "TODO"
 COPY ./ /app
 RUN npm install
--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -4,4 +4,9 @@ docker build -t northern-lights:local .
 docker kill northern-lights
 sleep 1
 docker rm northern-lights
-docker run -d --net=host --restart=always --name=northern-lights --log-driver=json-file --log-opt=max-size=10m --log-opt=max-file=5 northern-lights:local
+docker run -d --net=host --restart=always --name="northern-lights" \
+  -e "GW_IDENTITY=$(cat secret_GW_IDENTITY)" \
+  -e "GW_PSK=$(cat secret_GW_PSK)" \
+  -e "GW_ADDRESS=$(cat secret_GW_ADDRESS)" \
+  --log-driver=json-file --log-opt=max-size=10m --log-opt=max-file=5 \
+  northern-lights:local
